9d7b6962e09fba7f52af4c527541e55be874e991
doujin5 <abhi.monty143@gmail.com>
2018-03-24 08:49:09 +0530

    -a

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var path=require('path');
-
+var count = 0;
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendfile(path.join(__dirname,"../public/star.html"));
 });
+router.post('/',function (req,res) {
+	// body...
+	var u=req.body.username;
+	var p=req.body.password;
+	if(u=='zairza'&& p=='itssexx')
+		count++;
+		console.log(count);
+	res.send('/profile');
+});
+
+router.get('/get_count',function(req,res){
+	res.send(" "+count);
+})
 
 module.exports = router;
